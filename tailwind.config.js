c53581f9ead67ba33672c4542b68e7b4905cd491
Takim Ahmed <takimahmed1122@gmail.com>
2023-07-27 23:54:08 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  // Colors code 
+  module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'my-primary': '#F85559',
+        },
+      }
+    }
+  }
